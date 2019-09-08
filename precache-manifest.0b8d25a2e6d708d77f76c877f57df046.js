61649020ad5f2ac3880f979883dc1a78f03628c1
Ian Dahlberg <ijdahlberg@gmail.com>
2019-09-07 21:04:44 -0700

    Updates

---
+++ precache-manifest.0b8d25a2e6d708d77f76c877f57df046.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "457cbbb764b726dde13d349d2678c5fb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "963c542b123df1195428",
+    "url": "/static/css/main.d4c4d115.chunk.css"
+  },
+  {
+    "revision": "ebdf587097504289c06e",
+    "url": "/static/js/2.3adefb50.chunk.js"
+  },
+  {
+    "revision": "963c542b123df1195428",
+    "url": "/static/js/main.c74b5444.chunk.js"
+  },
+  {
+    "revision": "ee28c13eaedc37b1c2ec",
+    "url": "/static/js/runtime~main.082704b6.js"
+  }
+]);
